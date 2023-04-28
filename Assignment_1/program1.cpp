729a47121da794b86415d741ad9096aecfc9fb15
12srishti12 <96789205+12srishti12@users.noreply.github.com>
2023-04-28 21:06:11 +0530

    Create program1.cpp

---
+++ Assignment_1/program1.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"Hello, World!";
+    return 0;
+}
+
+https://www.hackerrank.com/challenges/cpp-hello-world/submissions/code/268374086
